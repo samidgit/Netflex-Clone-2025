cbdc73c62a0b5ba44c81b2b78d7d6b097421c7fc
samidgit <samijalene@gmail.com>
2025-02-19 22:42:08 -0500

    header and footer

--- npx/src/App.jsx
+++ npx/src/App.jsx
@@ -2,15 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Home from './pages/Home/Home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='App'>
-      <h1>hello</h1>
+    <>
+      <Home/>
 
-    </div>
+    </>
   );
 }
 
